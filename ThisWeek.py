b5e36a735f17093eec947b3bb264ea18aa9a487d
Black, Samuel Thomas <sam.black@gatech.edu>
2019-10-07 15:28:00 -0500

    part 2 of today added

--- ThisWeek.py
+++ ThisWeek.py
@@ -8,3 +8,16 @@ y = "Those who know %s and those who %s" % (binary, doNot)
 print(x)
 print(y)
 
+print("I said: %r.:" % x)
+print("I also said: '%s'." % y)
+
+hilarious = True
+jokeEvaluation = "Isn't that joke so funny?! %r"
+
+print(jokeEvaluation % hilarious)
+
+w = "This is the left side of..."
+e = "a string with a right side."
+print(w+e)
+
+# More printing fun
