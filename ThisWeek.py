1780bb36562a067e90340d7ac39296c19ab7a599
Black, Samuel Thomas <sam.black@gatech.edu>
2019-10-07 15:55:17 -0500

    part 4 of today added

--- ThisWeek.py
+++ ThisWeek.py
@@ -40,3 +40,10 @@ end12 = "r"
 
 print(end1 + end2 + end3 + end4 + end5 + end6)
 print(end7 + end8 + end9 + end10 + end11 + end12)
+
+# But wait! There's more:
+formatter = "%r %r %r %r"
+print(formatter % (1, 2, 3, 4))
+print(formatter % ("one", "two", "three", "four"))
+print(formatter % (True, False, False, True))
+print(formatter % (formatter, formatter, formatter, formatter))
