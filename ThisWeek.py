458bb0ab9a65bc5e6c2eeaa8ecfe2d4d644f0d2c
Black, Samuel Thomas <sam.black@gatech.edu>
2019-10-08 15:28:19 -0500

    2nd commit of 10-8

--- ThisWeek.py
+++ ThisWeek.py
@@ -54,3 +54,15 @@ print(formatter % (formatter, formatter, formatter, formatter))
 
 # why does %r sometimes give me single quotes around things?
 
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
